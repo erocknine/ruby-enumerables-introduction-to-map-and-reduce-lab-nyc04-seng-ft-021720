c3317dc53218a04275cea78b1a6157085fd1b984
erocknine <echeungsk@gmail.com>
2020-01-29 15:39:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
-# My Code here....
+def map_to_negativize (array)
+  new_array = []
+  index=0
+  while index < array.length do
+    new_array.push(array[index] * -1)
+    index+=1
+  end
+  new_array
+end
+
+def map_to_no_change (array)
+  new_array = []
+  index=0
+  while index < array.length do
+    new_array.push(array[index])
+    index+=1
+  end
+  new_array
+end
+
+def map_to_double (array)
+  new_array = []
+  index = 0
+  while index < array.length do
+    new_array.push(array[index] * 2)
+    index += 1
+  end
+  new_array
+end
+
+
+    
+    
+    
+    
+    
+    
