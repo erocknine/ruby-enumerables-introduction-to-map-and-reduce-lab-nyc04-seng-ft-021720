70f47b8441cec023ad940aead1c91fd50ff5935b
erocknine <echeungsk@gmail.com>
2020-01-29 15:54:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,6 +57,17 @@ def reduce_to_all_true (array)
   end
   return true
 end
+
+def reduce_to_any_true (array)
+  index=0
+  while index < array.length do
+    if array[index]
+      return true
+    end
+    index+=1
+  end
+  return true
+end
     
     
     
