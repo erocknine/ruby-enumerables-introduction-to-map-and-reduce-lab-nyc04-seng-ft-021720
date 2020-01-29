7e4257936e69dfc0966465ad788264ce004d769f
erocknine <echeungsk@gmail.com>
2020-01-29 15:39:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,8 +28,17 @@ def map_to_double (array)
   new_array
 end
 
+def map_to_double (array)
+  new_array = []
+  index = 0
+  while index < array.length do
+    new_array.push(array[index] ** 2)
+    index += 1
+  end
+  new_array
+end
 
-    
+
     
     
     
